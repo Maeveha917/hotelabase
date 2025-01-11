4248c59392a79855d67dd02893ba3a12dbdd0654
Maeve <dylan_hanley@protonmail.com>
2025-01-11 00:21:30 +0000

    can now insert into tables

--- createEntry.php
+++ createEntry.php
@@ -6,15 +6,27 @@
     //connect to db
     $db = new SQLite3("database.db");
 
-    /*$query = "INSERT INTO ".$table_name." WHERE ".$pkName." = ".$key.";";
+    $query = "INSERT INTO ".$table_name." Values (";
+    echo $table_name;
+    echo $query;
+    //treat the fields such that they can be inserted into the database as text if need be
+    for ($i = 0; $i<sizeof($fieldArray);$i++){
+        if (!is_numeric($fieldArray[$i])){
+            $fieldArray[$i] = "'".$fieldArray[$i]."'";
+            echo $fieldArray[$i];
+        }
+    }
+    $query .= implode(",",$fieldArray).");";
+    echo $query;
+
     $isSuccessful = $db->exec($query);
 
     if ($isSuccessful){
-        echo "Deleted Successfuly!";
+        echo "Created Successfuly!";
     }else{
 
-        echo "Error deleting entry from ".$table_name." where ".$pkName." = ".$key;
-    }*/
+        echo "Error creating new entry in".$table_name;
+    }
 
     //close database connection
     $db->close();
